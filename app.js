a5e5d0f2e70cd421c2dffa56faca7c28bfa58e41
emile roch <emile31.roch@gmail.com>
2017-10-25 10:45:57 +0200

    exercice 4 done

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-var app = angular.module('gemstore',[]);
+var app = angular.module("gemstore", []);
+var gem = [
+  { name: "Azurite", price: 2.95, canPurchase: true },
+  { name: "Bloodstone", price: 5.95, canPurchase: true },
+  { name: "Zircon", price: 3.95, canPurchase: true }
+];
+app.controller("StoreController", function() {
+  this.products = gem;
+});
